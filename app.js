5816410e2951019dd1502926c424cb406f84631f
jiasheng_ou <oujia6014@foxmail.com>
2019-06-21 09:22:32 +0800

    session和redisd的使用

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const json = require('koa-json') //json格式处理
 const onerror = require('koa-onerror') //错误处理
 const bodyparser = require('koa-bodyparser') //postdata处理
 const logger = require('koa-logger') //日志处理
+const session = require('koa-generic-session') //session
+const redisStore = require('koa-redis')
 
 //路由引入
 const index = require('./routes/index')
@@ -16,7 +18,7 @@ onerror(app)
 
 //postData处理 监听-拼接
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 
@@ -24,26 +26,46 @@ app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-// view处理
+//view处理
 app.use(views(__dirname + '/views', {
-  extension: 'pug'
+    extension: 'pug'
 }))
 
-// 日志格式
+//日志格式
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    //接收请求的时间
+    const start = new Date()
+    //处理请求中
+    await next()
+    //请求处理完的时间
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-// 路由处理
+//session redis配置
+app.keys = ['wzsda1asd123_']
+app.use(
+    session({
+        cookie: {
+            path: '/',
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
+        },
+        stroe: redisStore({
+            all: '127.0.0.1:6379'
+        })
+    })
+)
+
+
+//路由处理
 app.use(index.routes(), index.allowedMethods())
 app.use(user.routes(), user.allowedMethods())
 app.use(blog.routes(), blog.allowedMethods())
-// 错误处理
+
+//错误处理
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
+    console.error('server error', err, ctx)
+})
 
 module.exports = app
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,22 @@ const router = require('koa-router')()
 router.prefix('/api/user')
 
 router.post('/login', async function (ctx, next) {
-  const {username, password} = ctx.request.body
-  ctx.body = {
-    username,
-    password
-  }
+    const {username, password} = ctx.request.body
+    ctx.body = {
+        username,
+        password
+    }
+})
+
+router.get('/session', async function (ctx, next) {
+    if (ctx.session.viewCount == null) {
+        ctx.session.viewCount = 0
+    }
+    ctx.session.viewCount++
+    ctx.body = {
+        text: 'session-test',
+        viewCount: ctx.session.viewCount
+    }
 })
 
 module.exports = router
